b8f686cc16749e228039ec636b4c93f835b52216
Samyam Dabadi <samyamdw@gmail.com>
2021-03-21 12:53:52 +0545

    icon and back controlled

--- App.js
+++ App.js
@@ -1,41 +1,26 @@
-// In App.js in a new project
-
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./components/Home";
 import NewPage from "./components/NewPage";
-import { Image, Text } from "react-native";
-// import WebView from 'react-native-webview';
+import BottomNav from "./components/BottomNav";
 
 const Stack = createStackNavigator();
 
 const pos = {
-  headerStyle: {height: 160},
-  headerTitleAlign: "center",
-  headerTitle: (
-    // <Text>Aaha</Text>
-    // <Image
-    //   source={require("./assets/logo.png")}
-    //   style={{ height: 40, width: 100 }}
-    //   resizeMode="cover"
-    // />
-    <Image
-          style={{ height: 70, width:180 }}
-          source={{
-            uri:
-              "https://aahashop.com/wp-content/uploads/2021/03/Aaha-shop-logo.png",
-          }}
-        />
-  ),
-};
+  headerStyle: {height: 0},
+  headerTitle: "",
+  headerLeft: null
+}
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} options={pos} />
-        <Stack.Screen name="Aahashop" component={NewPage} options={{headerTitle: ""}} />
+        {/* <Stack.Screen name="Aahashop" component={NewPage} options={{ headerLeft: null }} /> */}
+        <Stack.Screen name="Aahashop" component={NewPage} options={pos} />
       </Stack.Navigator>
+      {/* <BottomNav style={{flex: .1}}/> */}
     </NavigationContainer>
 
     // <WebView source={{ uri: "https://aahashop.com/" }} />
